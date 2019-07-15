9c914958fbd0cec86b2e14ea6580ede345fb131e
blue42u <blue42u@users.noreply.github.com>
2019-07-14 22:47:22 -0500

    Initial attempts at performance output generation

---
+++ tests/perf/Tupfile.lua
@@ -0,0 +1,21 @@
+-- luacheck: std lua53, no global (Tup-Lua)
+
+tup.rule('../../reference/hpctoolkit/<bin>', '^o Generated %o^ sed'
+  ..[[ -e "/^libmonitor_dir/clibmonitor_dir='`realpath ../../external/monitor/lib`'"]]
+  ..[[ -e "/^libunwind_dir/clibunwind_dir='`realpath ../../external/unwind/lib`'"]]
+  ..[[ -e "/^papi_libdir/cpapi_libdir='`realpath ../../external/papi/lib`'"]]
+  ..[[ -e "/^perfmon_libdir/cperfmon_libdir='`realpath ../../external/papi/lib`'"]]
+  ..' ../../reference/hpctoolkit/install/bin/scripts/hpcrun > %o && chmod +x %o', 'hpcrun')
+
+forall(function()
+  return {
+    id = 'Perf', threads = 8,
+    deps = {
+      'hpcrun', '../../external/monitor/<build>',
+      '../../external/unwind/<build>', '../../external/papi/<build>',
+      '../../reference/hpctoolkit/<libs>'
+    },
+    cmd = './hpcrun -o %o.tmp %C && tar -C %o.tmp -cf ../%o . && LD_PRELOAD= rm -rf %o.tmp',
+    output = '%t.%i.run.8', serialize = true,
+  }
+end)
