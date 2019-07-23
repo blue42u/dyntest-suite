8a834ec67b9e15468f1490f29912b042c2ff2ff6
blue42u <blue42u@users.noreply.github.com>
2019-07-23 17:44:06 -0500

    Reenable the testing system. Haven't tested it yet, but its ready for merge

--- tests/stable/Tupfile.lua
+++ tests/stable/Tupfile.lua
@@ -1,29 +1,29 @@
 -- luacheck: std lua53, no global (Tup-Lua)
 
--- tup.rule(forall(function(i)
---   if i.size > 2 then return end
---   local runs = {}
---   for idx,c in ipairs{1,2,4,8,16,32} do
---     runs[idx] = {
---       id = 'Stable ('..c..')', threads = c, cmd = '%C',
---       output = '%t.%i/run.'..c, --serialize = true,
---     }
---   end
---   return {
---     id = 'Stable (ref)', reference = true, threads = 1, cmd = '%C',
---     output = '%t.%i/ref',
---   }, table.unpack(runs)
--- end, function(ins, i, t)
---   if #ins == 0 then return end
---   if t.outclean then
---     for idx, f in ipairs(ins) do
---       local o = f..'.clean'
---       tup.rule(f, '^o Cleaned %f^ '..t.outclean, o)
---       ins[idx] = o
---     end
---   end
---   ins[1] = ins[2]
---   local o = t.id..'.'..i.id..'.out'
---   tup.rule(ins, '^o Generated %o^ ./diffout.sh '..i.id..' '..t.id..' %f > %o', o)
---   return {o}
--- end), '^o Concatinated %o^ cat %f > %o', 'stable.out')
+tup.rule(forall(function(i)
+  if i.size > 2 then return end
+  local runs = {}
+  for idx,c in ipairs{1,2,4,8,16,32} do
+    runs[idx] = {
+      id = 'Stable ('..c..')', threads = c, cmd = '%C',
+      output = '%t.%i/run.'..c, --serialize = true,
+    }
+  end
+  return {
+    id = 'Stable (ref)', reference = true, threads = 1, cmd = '%C',
+    output = '%t.%i/ref',
+  }, table.unpack(runs)
+end, function(ins, i, t)
+  if #ins == 0 then return end
+  if t.outclean then
+    for idx, f in ipairs(ins) do
+      local o = f..'.clean'
+      tup.rule(f, '^o Cleaned %f^ '..t.outclean, o)
+      ins[idx] = o
+    end
+  end
+  ins[1] = ins[2]
+  local o = t.id..'.'..i.id..'.out'
+  tup.rule(ins, '^o Generated %o^ ./diffout.sh '..i.id..' '..t.id..' %f > %o', o)
+  return {o}
+end), '^o Concatinated %o^ cat %f > %o', 'stable.out')
