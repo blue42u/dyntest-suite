bf46b37b31ceff20404ddee308204ea776bdd328
blue42u <blue42u@users.noreply.github.com>
2019-08-07 14:36:51 -0500

    Add a crashtesting element to the suite. Whether it works or not I don't know.

---
+++ tests/crash/Tupfile.lua
@@ -0,0 +1,18 @@
+-- luacheck: std lua53, no global (Tup-Lua)
+
+sclass = 3
+
+local rep = 0
+if tup.getconfig 'CRASH_REP' ~= '' then
+  rep = assert(math.tointeger(tup.getconfig 'CRASH_REP'),
+    'Configuration option CRASH_REP must be a valid integer!')
+end
+if rep == 0 then return end
+
+tup.rule(forall(function()
+  return {
+    id = 'Crash', threads=maxthreads,
+    cmd = './rep.sh '..rep..' %o %C', redirect = '/dev/null',
+    output = 'crashes.%t.%i.log', serialize = true,
+  }
+end), '^o Concatinated %o^ cat %f > %o', 'crashes.log')
---
+++ tests/crash/rep.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+REP="$1"
+OUT="$2"
+shift 2
+
+set -e
+
+printf "SUMMARY: %${#REP}d / %d sucessful, %${#REP}d crashes.\n" 0 0 0 > "$OUT"
+
+GOOD=0
+BAD=0
+for ((i=0; i<"$REP"; i++)); do
+  # In our case, we assume the process will crash when it segfaults rather than
+  # using the output to check. Not quite right, but close enough.
+  if catchsegv "$@" >> "$OUT"
+  then GOOD=$((GOOD+1))
+  else BAD=$((BAD+1))
+  fi
+done
+echo >> "$OUT"
+
+printf "SUMMARY: %${#REP}d / %d sucessful, %${#REP}d crashes.\n" \
+  $GOOD $REP $BAD | dd status=none conv=notrunc bs=1 of="$OUT"
