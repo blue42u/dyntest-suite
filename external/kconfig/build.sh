1f6ce05a812ee10978547d4fe6a3b84dd797923c
blue42u <blue42u@users.noreply.github.com>
2019-09-11 09:46:59 -0500

    Finally fix some bits with kconfig

--- external/kconfig/build.sh
+++ external/kconfig/build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-source ../init.sh
+source ../init.sh http://deb.debian.org/debian/pool/main/k/kconfig-frontends/kconfig-frontends_4.11.0.1+dfsg.orig.tar.bz2 \
+  5462393596a42e6efed78e1b0a56fde75466dd50452394b841f2882a78000ca8 \
+  3a0a0a8e2a73f52a58864fbc212bb2d9
 
-# The released version has an old configure and so kinda breaks a bit.
-# We need autotools and libtool anyway, so just use the git.
-git clone git://ymorin.is-a-geek.org/kconfig-frontends kconfig
-cd kconfig
-autoreconf -fis &> /dev/null
+# Apply a hotfix patch
+patch -p1 < "$INSTALL"/../hotfixes.patch &> /dev/null
 
 # We need access to gperf, so stick it in the PATH
 export PATH="$INSTALL"/../../gperf/install/bin:"$PATH"
 
 # The usual configure-make-install
+autoreconf -fis &> /dev/null
 ./configure --prefix="`realpath zzz`" --quiet \
   --disable-utils --disable-kconfig  > /dev/null
 make --quiet > /dev/null
