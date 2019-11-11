783691dfc43bbc57c7b421d0e72045587e077683
blue42u <blue42u@users.noreply.github.com>
2019-11-11 13:50:13 -0600

    Convert parvecsum to use Pthreads, so it works just about everywhere

--- inputs/src/parvecsum.c
+++ inputs/src/parvecsum.c
@@ -1,10 +1,7 @@
-#include <threads.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 #include <pthread.h>
-#define PTHREAD
 
 void sum(size_t* restrict a, size_t* restrict b, size_t n) {
   for(size_t i = 0; i < n; i++) {
@@ -28,55 +25,38 @@ void print(const size_t* a, size_t n) {
     printf(" %zu", a[i]);
 }
 
-mtx_t a_lock;
+pthread_mutex_t a_lock;
 size_t* a;
 
 size_t n = 1ul << 15;
 
-#ifdef PTHREAD
-void*
-#else
-int
-#endif
-worker(void* _) {
+void* worker(void* _) {
   size_t* b = malloc(n * sizeof *b);
   fill3(b, n);
-  mtx_lock(&a_lock);
+  pthread_mutex_lock(&a_lock);
   struct timespec zs = {0, 500000000};
   nanosleep(&zs, NULL);
   sum(a, b, n);
-  mtx_unlock(&a_lock);
+  pthread_mutex_unlock(&a_lock);
   free(b);
-  return 0;
+  return NULL;
 }
 
 int main() {
   a = malloc(n * sizeof *a);
   fill2(a, n);
-  mtx_init(&a_lock, mtx_plain);
-  mtx_lock(&a_lock);
+  pthread_mutex_init(&a_lock, NULL);
+  pthread_mutex_lock(&a_lock);
 
-#ifdef PTHREAD
   pthread_t
-#else
-  thrd_t
-#endif
   ts[4];
   for(int i = 0; i < sizeof ts / sizeof ts[0]; i++)
-#ifdef PTHREAD
     pthread_create(&ts[i], NULL, worker, NULL);
-#else
-    thrd_create(&ts[i], worker, NULL);
-#endif
-  mtx_unlock(&a_lock);
+  pthread_mutex_unlock(&a_lock);
   for(int i = 0; i < sizeof ts / sizeof ts[0]; i++)
-#ifdef PTHREAD
     pthread_join(ts[i], NULL);
-#else
-    thrd_join(ts[i], NULL);
-#endif
 
-  mtx_destroy(&a_lock);
+  pthread_mutex_destroy(&a_lock);
   print(a, n);
   free(a);
   return 0;
