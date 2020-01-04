147ffe234a2498ef2f6e89d6243bbcc0150d73a6
blue42u <anderson.jonathonm@gmail.com>
2020-01-04 08:17:43 -0600

    Updates and minor fixes

--- src/testsuite.lua
+++ src/testsuite.lua
@@ -2,18 +2,23 @@
 
 function testsuite() end
 
-do return end
-
 tup.include '../build/build.lua'
-function testsuite(o) return build {
-  srcdir = 'src/testsuite',
-  builddir = o.builddir,
-  cfgflags = [[
-    -DDyninst_SRC_DIR=@@/src/dyninst
-    -DDyninst_ROOT=@]]..o.dyninst..[[@
-    -DElfUtils_ROOT_DIR=@]]..o.elfutils..[[@
-    -DTBB_ROOT_DIR=@/external/tbb@
-    -DBoost_ROOT_DIR=@/external/boost@
-    -DLibXml2_ROOT=@/external/libxml@
-  ]]..(o.cfg or ''),
-} end
+function testsuite(o)
+  local r = {build {
+    srcdir = 'src/testsuite',
+    builddir = o.builddir,
+    cfgflags = [[
+      -DDyninst_SRC_DIR=@@/src/dyninst
+      -DDyninst_ROOT=@]]..o.dyninst..[[@
+      -DElfUtils_ROOT_DIR=@]]..o.elfutils..[[@
+      -DTBB_ROOT_DIR=@/external/tbb@
+      -DBoost_ROOT_DIR=@/external/boost@
+      -DLibXml2_ROOT=@/external/libxml@
+    ]]..(o.cfg or ''),
+  }}
+
+  -- Symlink install/lib to install/bin/testsuite, because someone had a bright idea.
+  tup.rule([[^o Linked %o^ ln -s bin/testsuite %o]], {'install/lib', '<build>'})
+
+  return table.unpack(r)
+end
